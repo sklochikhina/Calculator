b66a3973359b9c1c1e239afd87435fa5f65a6603
sklochikhina <klochikhina.sofya@mail.ru>
2024-01-04 22:21:25 +0800

    :D

--- src/ru/nsu/klochikhina/calculator/model/commands/Comment.java
+++ src/ru/nsu/klochikhina/calculator/model/commands/Comment.java
@@ -6,5 +6,7 @@ import java.util.List;
 
 public class Comment implements Command {
     @Override
-    public void action(List<String> list) throws Exception{}
+    public void action(List<String> list) throws Exception{
+        // тут нет и не будет ничего интересного :D
+    }
 }
